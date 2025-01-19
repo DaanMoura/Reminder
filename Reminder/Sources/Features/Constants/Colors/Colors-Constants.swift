9bc0967878c7ea8065b40e4f8ef6f5be9efb41c5
Daniel Moura <daniel.lucio.moura@gmail.com>
2025-01-18 23:57:20 -0300

    feat: init splash screen

---
+++ Reminder/Sources/Features/Constants/Colors/Colors-Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Colors-Constants.swift
+//  Reminder
+//
+//  Created by Daniel Moura on 18/01/25.
+//
+
+import Foundation
+
--- Reminder/Sources/Features/Splash/SplashView.swift
+++ Reminder/Sources/Features/Splash/SplashView.swift
@@ -10,8 +10,27 @@ import UIKit
 
 class SplashView: UIView {
     
-    init() {
-        super.init(frame: .zero)
+    private let logoImageView: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "Logo")
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    private let titleLogoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Reminder"
+//        label.font = .systemFont(ofSize: 32, weight: .bold)
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+//        label.textColor = .white
+//        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         setupUI()
     }
     
@@ -20,11 +39,21 @@ class SplashView: UIView {
     }
     
     private func setupUI() {
-        self.backgroundColor = .blue
+        self.backgroundColor = .systemRed
+        self.addSubview(logoImageView)
+        self.addSubview(titleLogoLabel)
         setupConstraints()
     }
     
     private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            titleLogoLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: 24),
+            titleLogoLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            
+            logoImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            logoImageView.trailingAnchor.constraint(equalTo: titleLogoLabel.leadingAnchor, constant: -8)
+        ])
         
+        self.translatesAutoresizingMaskIntoConstraints = false
     }
 }
