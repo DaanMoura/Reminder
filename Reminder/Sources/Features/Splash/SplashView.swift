4acbe05b479f789921cca69bc2aee2f10303db5e
Daniel Moura <daniel.lucio.moura@gmail.com>
2025-09-27 00:14:06 -0300

    feat: init bottom sheet for login

--- Reminder/Sources/Features/Splash/SplashView.swift
+++ Reminder/Sources/Features/Splash/SplashView.swift
@@ -9,51 +9,35 @@ import Foundation
 import UIKit
 
 class SplashView: UIView {
+  private let logoImageView: UIImageView = {
+    let image = UIImageView()
+    image.image = UIImage(named: "Logo")
+    image.contentMode = .scaleAspectFit
+    image.translatesAutoresizingMaskIntoConstraints = false
+    return image
+  }()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func setupUI() {
+    self.backgroundColor = Colors.primaryRedBase
+    self.addSubview(logoImageView)
+    setupConstraints()
+  }
+  
+  private func setupConstraints() {
+    NSLayoutConstraint.activate([
+      logoImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+      logoImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+    ])
     
-    private let logoImageView: UIImageView = {
-        let image = UIImageView()
-        image.image = UIImage(named: "Logo")
-        image.contentMode = .scaleAspectFit
-        image.translatesAutoresizingMaskIntoConstraints = false
-        return image
-    }()
-    
-    private let titleLogoLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Reminder"
-//        label.font = .systemFont(ofSize: 32, weight: .bold)
-        label.font = UIFont.boldSystemFont(ofSize: 32)
-//        label.textColor = .white
-//        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupUI()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setupUI() {
-        self.backgroundColor = .systemRed
-        self.addSubview(logoImageView)
-        self.addSubview(titleLogoLabel)
-        setupConstraints()
-    }
-    
-    private func setupConstraints() {
-        NSLayoutConstraint.activate([
-            titleLogoLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: 24),
-            titleLogoLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            
-            logoImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            logoImageView.trailingAnchor.constraint(equalTo: titleLogoLabel.leadingAnchor, constant: -8)
-        ])
-        
-        self.translatesAutoresizingMaskIntoConstraints = false
-    }
+    self.translatesAutoresizingMaskIntoConstraints = false
+  }
 }
--- Reminder/Sources/Features/Splash/SplashViewController.swift
+++ Reminder/Sources/Features/Splash/SplashViewController.swift
@@ -9,33 +9,51 @@ import Foundation
 import UIKit
 
 class SplashViewController: UIViewController {
-    // TODO: add it by dependency injection
-    let contentView = SplashView()
+  // TODO: add it by dependency injection
+  let contentView = SplashView()
 
-    // Very important lifecycle method:
-    // First things that will be executed whe the view is loaded
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        setup()
-    }
+  // Very important lifecycle method:
+  // First things that will be executed whe the view is loaded
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    private func setup() {
-        self.view.addSubview(contentView)
-        
-        setupConstraints()
-    }
+    setup()
+    setupGesture()
+  }
+  
+  private func setup() {
+    self.view.addSubview(contentView)
+    self.navigationController?.navigationBar.isHidden = true
+    self.view.backgroundColor = Colors.primaryRedBase
     
-    private func setupConstraints() {
-        NSLayoutConstraint.activate([
-            contentView.topAnchor.constraint(equalTo: view.topAnchor),
-            contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-        ])
-       
-        // Always set this to false on UIKit
-        contentView.translatesAutoresizingMaskIntoConstraints = false
+    setupConstraints()
+  }
+  
+  private func setupConstraints() {
+    NSLayoutConstraint.activate([
+      contentView.topAnchor.constraint(equalTo: view.topAnchor),
+      contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+    ])
+   
+    // Always set this to false on UIKit
+    contentView.translatesAutoresizingMaskIntoConstraints = false
+  }
+  
+  private func setupGesture() {
+    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(showLoginBottomSheet))
+    self.view.addGestureRecognizer(tapGesture)
+  }
+  
+  @objc
+  private func showLoginBottomSheet() {
+    let loginBottomSheet = LoginBottomSheetViewController()
+    loginBottomSheet.modalPresentationStyle = .overCurrentContext
+    loginBottomSheet.modalTransitionStyle = .crossDissolve
+    self.present(loginBottomSheet, animated: false) {
+      loginBottomSheet.animateShow()
     }
+  }
 }
 
